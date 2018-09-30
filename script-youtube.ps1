fe7ca60a46bf3cc93cce9294286393501e4cee4d
Aitor León Delgado <ldaitor28@gmail.com>
2018-09-30 12:52:38 +0200

    Some bugs fixed

--- script-youtube.ps1
+++ script-youtube.ps1
@@ -12,6 +12,7 @@ $directorio = "C:\Users\$usuario1\Desktop\Youtube"
 $Comprobacion = Test-Path $directorio 
 
 #Si no existe, descargamos y descomprimimos los archivos necesarios
+
 If ($Comprobacion -eq $False) {
 
     # Creamos el directorio Youtube en el escritorio
@@ -19,8 +20,9 @@ If ($Comprobacion -eq $False) {
     mkdir Youtube
 
     # Definimos la url del archivo youtube-dl y su directorio.
-
-    $url = "https://yt-dl.org/downloads/2017.10.01/youtube-dl.exe"
+    
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $url = "https://github.com/rg3/youtube-dl/releases/download/2018.09.26/youtube-dl.exe"
     $path = "C:\Users\$usuario1\Desktop\Youtube\youtube-dl.exe"
 
     $youtube = New-Object System.Net.WebClient
@@ -28,27 +30,27 @@ If ($Comprobacion -eq $False) {
 
     # Definimos la url del convertidor ffmpeg y su directorio.
 
-    $url1 = "http://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20170921-183fd30-win64-static.zip"
-    $path1 = "C:\Users\$usuario1\Desktop\Youtube\ffmpeg-20170921-183fd30-win64-static.zip"
+    $url1 = "https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20180930-b577153-win64-static.zip"
+    $path1 = "C:\Users\$usuario1\Desktop\Youtube\ffmpeg-20180930-b577153-win64-static.zip"
 
     $ffmpeg = New-Object System.Net.WebClient
     $ffmpeg.DownloadFile($url1, $path1)
 
     # Descomprimos el zip
-    Expand-Archive "C:\Users\$usuario1\Desktop\Youtube\ffmpeg-20170921-183fd30-win64-static.zip" -DestinationPath "C:\Users\$usuario1\Desktop\Youtube\"
+    Expand-Archive "C:\Users\$usuario1\Desktop\Youtube\ffmpeg-20180930-b577153-win64-static.zip" -DestinationPath "C:\Users\$usuario1\Desktop\Youtube\"
 
     # Movemos los ejecutables dentro del directorio Bin al directorio padre Youtube.
-    Move-Item "C:\Users\$usuario1\Desktop\Youtube\ffmpeg-20170921-183fd30-win64-static\bin\*" "C:\Users\$usuario1\Desktop\Youtube\"
+    Move-Item "C:\Users\$usuario1\Desktop\Youtube\ffmpeg-20180930-b577153-win64-static\bin\*" "C:\Users\$usuario1\Desktop\Youtube\"
 
     #Eliminamos el directorio resultante de la descomprención y el fichero .zip
     cd "C:\Users\$usuario1\Desktop\Youtube"
-    rm ffmpeg-20170921-183fd30-win64-stati* -Recurse -Force
+    rm ffmpeg-20180930-b577153-win64-stati* -Recurse -Force
 
     # Preguntamos por la url a descargar
     $video = Read-Host -Prompt "Pega la URL"
 
     $ruta = pwd
-    $posicionpredeterminada = "C:\Users\$usuario1\Desktop\Youtube"
+    $posicionpredeterminada = "C:\Users\$usuario1\Desktop\Youtube\"
 
     If ($ruta -eq $posicionpredeterminada) {
     
@@ -56,7 +58,7 @@ If ($Comprobacion -eq $False) {
         .\youtube-dl.exe --extract-audio --audio-format mp3 --audio-quality 0 $video
     }
     else {
-        cd "C:\Users\$usuario1\Desktop\Youtube"
+        cd "C:\Users\$usuario1\Desktop\Youtube\"
         # Ejecutamos youtube-dl
         .\youtube-dl.exe --extract-audio --audio-format mp3 --audio-quality 0 $video
     }
@@ -80,3 +82,4 @@ else {
         .\youtube-dl.exe --extract-audio --audio-format mp3 --audio-quality 0 $video
     }
 }
+#>
